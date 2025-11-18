f3ed786b4c180009d30e60cbdc62cce111d543fb
roterfile81 <1DAM@A11-PC06>
2025-11-18 12:33:26 +0100

    funciones 3

--- Reto2AlejandroIzanNabil/src/Reto/Funciones3.java
+++ Reto2AlejandroIzanNabil/src/Reto/Funciones3.java
@@ -1,30 +1,59 @@
 package Reto;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Funciones3 {
-	
-	Scanner sc = new Scanner(System.in);
-	String op = sc.nextLine();
-	int opcion = Integer.parseInt(op);
 
+	public static double moneda() {
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+		System.out.println("---Menu eurodolar---");
+		System.out.println("1. euro a dolar");
+		System.out.println("2. dolar a euro");
+		int n1 = Integer.parseInt(sc.next());
+		switch (n1) {
+		case 1:
+			System.out.println("dame el importe en euros ");
+			double euro = Double.parseDouble(sc.nextLine());
+			double dolar = euro * 1.08;
+			 System.out.println(dolar);
+			break;
+		case 2:
+			System.out.println("dame el importe en dolares ");
+			double dolar2 = Double.parseDouble(sc.nextLine());
+			double euro2 = dolar2 / 1.08;
+			 System.out.println(euro2);
+			break;
 
-	 public static double eurodolar(int n1) {
-		 return n1*1.08;
-		
-	 }
-	 public static double dolareuro(int n1) {
-		 return n1/1.08;
-		 
-	 }
-	 
-	 public static double gradosradianes(int n1) {
-		 return n1* Math.PI / 180;
-		 }
-	 public static double radianesgrados(int n1) {
-		 return n1* 180 / Math.PI;
-		 }
+		default: System.out.println("errord");
+			break;
+		}
+		return 0;
+	}
+	public static double angulo() {
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+		System.out.println("---Menu angulo---");
+		System.out.println("1. grados a radianes");
+		System.out.println("2. radianes a grados");
+		int n1 = Integer.parseInt(sc.next());
+		switch (n1) {
+		case 1:
+			System.out.println("dame los grados ");
+			double grado = Double.parseDouble(sc.nextLine());
+			double radianes = grado * Math.PI / 180;
+			 System.out.println(radianes);
+			break;
+		case 2:
+			System.out.println("dame los radianes ");
+			double radianes2 = Double.parseDouble(sc.nextLine());
+			double grado2 = radianes2 * 180 / Math.PI;
+			 System.out.println(grado2);
+			break;
 
-	 
-	 
+		default: System.out.println("errord");
+			break;
+		}
+		return 0;
+	}
 }
+
