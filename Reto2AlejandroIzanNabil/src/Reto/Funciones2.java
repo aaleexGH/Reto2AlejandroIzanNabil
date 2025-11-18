570a38d80b18aeffca957b425de085b5fe051087
1DAM <1DAM@A11-PC18>
2025-11-18 11:51:09 +0100

    funciones temperautra

--- Reto2AlejandroIzanNabil/src/Reto/Funciones2.java
+++ Reto2AlejandroIzanNabil/src/Reto/Funciones2.java
@@ -1,5 +1,14 @@
 package Reto;
 
 public class Funciones2 {
+	public static double celsiusFahrenheit(double celsius) {
+		double fahrenheit = (double) (celsius * 9.0 / 5.0 + 32);
+		return fahrenheit;
+	}
+	public static double fahrenheitCelsius(double fahrenheit) {
+	double celsius = (fahrenheit - 32) * 5.0 / 9.0;
+	return celsius;
+	}
 
 }
+
