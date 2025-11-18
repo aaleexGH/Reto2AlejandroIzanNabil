46c19f69ea05ee506d6bce232a79fe97e2d7e5a7
1DAM <1DAM@A11-PC18>
2025-11-18 12:13:25 +0100

    funciones 2?

--- Reto2AlejandroIzanNabil/src/Reto/Funciones2.java
+++ Reto2AlejandroIzanNabil/src/Reto/Funciones2.java
@@ -1,14 +1,28 @@
 package Reto;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Funciones2 {
-	public static double celsiusFahrenheit(double celsius) {
-		double fahrenheit = (double) (celsius * 9.0 / 5.0 + 32);
-		return fahrenheit;
-	}
-	public static double fahrenheitCelsius(double fahrenheit) {
-	double celsius = (fahrenheit - 32) * 5.0 / 9.0;
-	return celsius;
-	}
 
+	public static double temperatura() {
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+		System.out.println("---Menu termperatura---");
+		System.out.println("1. Celsius a Fahrentheit");
+		System.out.println("2. Fahrentheit a Celsius");
+		int n1 = Integer.parseInt(sc.next());
+		switch (n1) {
+		case 1:
+			System.out.println("dame la temperatura en celsius: ");
+			double celsius = Double.parseDouble(sc.nextLine());
+			double Fahrenheit = celsius * 9.0 / 5.0 + 32;
+			 System.out.println(Fahrenheit);
+			break;
+
+		default:
+			break;
+			
+		}
+		return n1;
+	}
 }
-
